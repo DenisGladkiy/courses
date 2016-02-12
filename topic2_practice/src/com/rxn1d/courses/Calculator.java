17916a6bd6217013e520ae2efc1fee2e5ad7f19b
Denis <gladkiy@gmail.com>
2016-02-12 23:30:50 +0200

    Calculator commit

--- topic2_practice/src/com/rxn1d/courses/Calculator.java
+++ topic2_practice/src/com/rxn1d/courses/Calculator.java
@@ -4,11 +4,39 @@ package com.rxn1d.courses;
  * @author Ievgen Tararaka
  */
 public class Calculator {
+    static String[] in;
+    static double res;
     public static void main(String[] args) {
         System.out.println("---> START Calculator application <---");
 
         // для того, чтобы читать данные из консоли спользуйте данную конструкцию
-        String[] in = ConsoleReader.readFromConsole();
+        in = ConsoleReader.readFromConsole();
+        double a = Double.valueOf(in[0]);
+        double b = Double.valueOf(in[2]);
+        double currRes = count(a, in[1], b);
+        if(in.length > 3){
+            for(int i = 3; i < in.length; i+=2){
+                double temp = currRes;
+                currRes = count(temp, in[i], Double.valueOf(in[i+1]));
+            }
+        }
+        res = currRes;
+        System.out.println("Result = "+res);
         System.out.println("---> EXIT Calculator application <---");
+    }
+    public static double count(double a, String sign, double b){
+        if(sign.equals("+")){
+            res = a + b;
+        }else if(sign.equals("-")){
+            res = a - b;
+        }else if(sign.equals("*")){
+            res = a * b;
+        }else if(sign.equals("/")){
+            res = a / b;
+        }else{
+            res = 0;
+            System.out.println("Unknown operation");
+        }
+        return res;
     }
 }
