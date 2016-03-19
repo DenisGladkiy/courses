8347491fe4d2cbc32a38938752db93765f766a5e
DenisGladkiy <gladkiy@gmail.com>
2016-03-19 10:44:50 +0200

    FileReader

--- topic10/src/main/java/com/courses/spalah/FileReader.java
+++ topic10/src/main/java/com/courses/spalah/FileReader.java
@@ -1,10 +1,15 @@
 package com.courses.spalah;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 /**
  * @author Ievgen Tararaka
  */
 public class FileReader {
     public String pathToFile;
+    private String line;
 
     public FileReader(String pathToFile) {
         this.pathToFile = pathToFile;
@@ -15,7 +20,19 @@ public class FileReader {
      * @return content of file
      */
     public String readFile() {
-        return "";
+        StringBuilder builder = new StringBuilder();
+        try {
+            BufferedReader reader = new BufferedReader(new java.io.FileReader(pathToFile));
+            line = reader.readLine();
+            while(line != null){
+                builder.append(line + "\n");
+                line = reader.readLine();
+            }
+            reader.close();
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+        }
+        return builder.toString();
     }
 
     public String getPathToFile() {
@@ -25,4 +42,5 @@ public class FileReader {
     public void setPathToFile(String pathToFile) {
         this.pathToFile = pathToFile;
     }
+
 }
