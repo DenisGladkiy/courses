c8a24e9ad08c8a5c88859039e5e03939bc494054
DenisGladkiy <gladkiy@gmail.com>
2016-03-30 10:18:35 +0300

    Insertion revised with autoincrement

--- topic12/src/main/java/TableRows.java
+++ topic12/src/main/java/TableRows.java
@@ -10,6 +10,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -20,17 +21,17 @@ public class TableRows {
     private Connection connection;
     private DaoFactory daoFactory;
 
-    public String[]  getTableRowById(int id){
-        String sql = "SELECT * FROM carmarket.advert "+
+    public String[] getTableRowById(int id) {
+        String sql = "SELECT * FROM carmarket.advert " +
                 " INNER JOIN  carmarket.car ON carmarket.advert.idcar = " +
-                "carmarket.car.idcar INNER JOIN carmarket.owner ON carmarket.car.idowner = carmarket.owner.idowner" ;
+                "carmarket.car.idcar INNER JOIN carmarket.owner ON carmarket.car.idowner = carmarket.owner.idowner";
         daoFactory = new DaoFactory();
         String[] row = new String[7];
         try {
             connection = daoFactory.getConnection();
             Statement statement = connection.createStatement();
             ResultSet rs = statement.executeQuery(sql);
-            while (rs.next()){
+            while (rs.next()) {
                 //System.out.println(rs.getInt(1)+" "+rs.getInt(2)+" test");
                 row[0] = String.valueOf(rs.getInt(1));
                 row[1] = String.valueOf(rs.getInt(2));
@@ -47,25 +48,42 @@ public class TableRows {
         return row;
     }
 
-    public String [][] getAllRows(){
+    public String[][] getAllRows() {
+        String sql = "SELECT * FROM carmarket.advert " +
+                " INNER JOIN  carmarket.car ON carmarket.advert.idcar = " +
+                "carmarket.car.idcar INNER JOIN carmarket.owner ON carmarket.car.idowner = carmarket.owner.idowner";
         String[][] allRows = null;
         daoFactory = new DaoFactory();
-        String[] row = null;
+        String[] row = new String[7];
         try {
             connection = daoFactory.getConnection();
             AdvertDao aDao = daoFactory.getAdvertDao(connection);
             List<AdvertEntity> adverts = aDao.findAll();
             allRows = new String[adverts.size()][];
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery(sql);
             int index = 0;
-            for(AdvertEntity ae : adverts){
-                row = getTableRowById(ae.getCarId());
+            while (rs.next()) {
+                String manufacturer = rs.getString(7);
+                String model = rs.getString(8);
+                String year = String.valueOf(rs.getInt(6));
+                String vin = rs.getString(9);
+                String description = rs.getString(10);
+                String price = String.valueOf(rs.getInt(3));
+                String contact = rs.getString(12) + " " + rs.getString(13) + " " + String.valueOf(rs.getInt(2));
+                row[0] = manufacturer;
+                row[1] = model;
+                row[2] = year;
+                row[3] = vin;
+                row[4] = description;
+                row[5] = price;
+                row[6] = contact;
                 allRows[index] = row;
                 index++;
             }
         } catch (SQLException e) {
-        e.printStackTrace();
-    }
-
+            e.printStackTrace();
+        }
         return allRows;
     }
 }
