d525e35080267d0df067c3289eb9882705bf924f
DenisGladkiy <gladkiy@gmail.com>
2016-04-05 12:29:23 +0300

    Topic14 tomcat deploy test

--- topic14/src/main/java/com/courses/spalah/TestHttpServlet.java
+++ topic14/src/main/java/com/courses/spalah/TestHttpServlet.java
@@ -1,5 +1,9 @@
 package com.courses.spalah;
 
+import dao.CarDao;
+import dao.DaoFactory;
+import entity.CarEntity;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -10,6 +14,9 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
 
 /**
  * @author Ievgen Tararaka
@@ -17,7 +24,19 @@ import java.io.PrintWriter;
 public class TestHttpServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().write("<html><body>GET response</body></html>");
+        DaoFactory daoFactory = new DaoFactory();
+        List<CarEntity> carList = null;
+        try {
+            Connection connection = daoFactory.getConnection();
+            CarDao carDao = daoFactory.getCarDao(connection);
+            carList = carDao.findAll();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        for(CarEntity car : carList) {
+            //response.getWriter().write("<html><body>GET response</body></html>");
+            response.getWriter().write(car.toString());
+        }
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
