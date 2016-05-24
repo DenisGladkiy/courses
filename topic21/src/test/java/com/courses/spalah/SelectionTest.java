a478383f3c177fad422f5271fd0c5a171418067f
DenisGladkiy <gladkiy@gmail.com>
2016-05-24 23:32:55 +0300

    Topic 21 test added

--- topic21/src/test/java/com/courses/spalah/SelectionTest.java
+++ topic21/src/test/java/com/courses/spalah/SelectionTest.java
@@ -1,9 +1,15 @@
 package com.courses.spalah;
 
+import com.courses.spalah.entity.AdvertEntity;
+import com.courses.spalah.hibernate.HibernateUtil;
 import com.courses.spalah.stuff.SortRows;
+import org.hibernate.Query;
+import org.hibernate.Session;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -19,6 +25,18 @@ public class SelectionTest {
         String[] selection = new String[]{"testcar","testmodel","","",""};
         String[][] testSelect = sort.sortTable(selection);
         assertEquals(1, testSelect.length);
+        removeTestRow();
+    }
+    private void removeTestRow() {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        String hql_select = "from AdvertEntity WHERE car.manufacturer = 'testcar'";
+        Query query = session.createQuery(hql_select);
+        List<AdvertEntity> adverts = query.list();
+        int ownerId = adverts.get(0).getCar().getOwner().getIdowner();
+        String hql = "delete from OwnerEntity where idowner = " + ownerId;
+        Query removeQuery = session.createQuery(hql);
+        removeQuery.executeUpdate();
+        session.close();
     }
 
 }
