df8e58a3fee14c9e08b08107e93a9141c67b9df7
DenisGladkiy <gladkiy@gmail.com>
2016-05-24 22:11:06 +0300

    Topic 21 test added

--- topic21/src/test/java/com/courses/spalah/SelectionTest.java
+++ topic21/src/test/java/com/courses/spalah/SelectionTest.java
@@ -1,7 +1,11 @@
 package com.courses.spalah;
 
+import com.courses.spalah.stuff.SortRows;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by Денис on 5/22/16.
@@ -9,4 +13,12 @@ import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
 public class SelectionTest {
+    @Test
+    public void testSelection(){
+        SortRows sort = new SortRows();
+        String[] selection = new String[]{"testcar","testmodel","","",""};
+        String[][] testSelect = sort.sortTable(selection);
+        assertEquals(1, testSelect.length);
+    }
+
 }
